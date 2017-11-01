d86cdedd57785a0483b6bb26cdc6f4b1507effc0
matheuspinheiro <matheuspinheiro.lasalle@gmail.com>
2017-11-01 15:31:28 +0000

    Aula Hoje

--- backup.sh
+++ backup.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 arq=$1
-num=$(wc -l $arq)
 
-if [ $num -lt 100 ]; then
-mv $arq ./pequeno
+num=$( cat "$arq" | wc -l )
+
+
+if [[ $num -lt 100 ]]; then
+	mv $arq ./pequena
 elif [[ $num -gt 100 ]] && [[ $num -lt 500 ]]
-then
-mv $arq ./medio
-else
-mv $arq ./grande
+	then
+	mv $arq ./media
+	else
+	mv $arq ./grande 
 fi
+
+echo -e "\n O arquivo $arq, tem $num linhas, foi movido para a pasta correta com sucesso! \n"
 
