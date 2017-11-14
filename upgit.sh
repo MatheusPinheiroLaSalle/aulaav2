4d2c9b64920b3f7c39c26e4bf1368b131f777c57
MatheusPinheiroLaSalle <matheuspinheiro.lasalle@gmail.com>
2017-11-14 19:49:30 -0200

    Aula Hoje

--- upgit.sh
+++ upgit.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+git config --global user.name "MatheusPinheiroLaSalle"
+git config --global user.email "matheuspinheiro.lasalle@gmail.com"
+
 git add .
 git commit -m "Aula Hoje"
 git push
